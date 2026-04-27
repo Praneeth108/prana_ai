9646bacc4a6df5c494494029f535a0bb2fa0d6b8
praveen5506 <pravvenpasindu@gmail.com>
2026-04-28 01:20:27 +0530

    Manual coding: Fully completed Electric Shock Guide with all project files

---
+++ MY FIRST APP/MY FIRST APP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MY FIRST APP
+//
+//  Created by Praveen on BE 2569-03-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MY FIRST APP/MY FIRST APP/MY_FIRST_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  MY_FIRST_APPApp.swift
+//  MY FIRST APP
+//
+//  Created by Praveen on BE 2569-03-09.
+//
+
+import SwiftUI
+
+@main
+struct MY_FIRST_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
